794bbe77be6e4806064e3bd3438e6b7de67822fd
joao_gama <joao.gama@ssys.com>
2024-04-05 22:10:18 -0300

    paginação

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -3,7 +3,7 @@ class DebtsController < ApplicationController
 
   # GET /debts or /debts.json
   def index
-    @debts = Debt.limit(100)
+    @debts = Debt.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /debts/new

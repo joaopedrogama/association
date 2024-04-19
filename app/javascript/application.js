10d671606419c9c191cfdccb7cf8b5bab7de389b
joao_gama <joao.gama@ssys.com>
2024-04-19 20:56:28 -0300

    desenvolvido dashboard

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "chartkick"
-import "Chart.bundle"
 import * as bootstrap from "bootstrap"
 
 import { Application } from '@hotwired/stimulus'
 import { Autocomplete } from 'stimulus-autocomplete'
+
+import "chartkick"
+import "Chart.bundle"
 
 const application = Application.start()
 application.register('autocomplete', Autocomplete)

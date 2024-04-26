b87bff05d31ef5c602a7c489ca4446998ed02e1c
joao_gama <joao.gama@ssys.com>
2024-04-26 20:27:40 -0300

    removendo codigos duplicados

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,2 +1,14 @@
 module PaymentsHelper
+
+    def display_operation_errors(operation)
+        if operation.errors.any?
+          content_tag(:div, class: "alert alert-danger") do
+            concat content_tag(:h2, "#{pluralize(operation.errors.count, 'error')} prohibited this #{operation.class.name.underscore} from being saved:")
+            for error in operation.errors
+              concat content_tag(:p, error.full_message)
+            end
+          end
+        end
+    end
+
 end

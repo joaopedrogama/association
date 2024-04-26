7c821ed9555678985ad667875717217447276b97
joao_gama <joao.gama@ssys.com>
2024-04-26 19:47:29 -0300

    adicionando helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def display_active_status(object)
+      if object.active
+        content_tag(:span, class: "text-success") do
+          content_tag(:i, "", class: "bi bi-check-circle")
+        end
+      else
+        content_tag(:span, class: "text-danger") do
+          content_tag(:i, "", class: "bi bi-x-circle")
+        end
+      end
+    end
 end
